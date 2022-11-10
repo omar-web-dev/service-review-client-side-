e1156ce99130ab8d73960070e5d797f7732ff79d
omarwebdeve <omarf977865@gmail.com>
2022-11-10 20:43:47 +0600

    review update

--- src/pages/Review/MyReviewsCard.jsx
+++ src/pages/Review/MyReviewsCard.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaStarOfDavid } from 'react-icons/fa';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
+import ReactModal from 'react-modal';
+import StarsRating from 'stars-rating'
 
+
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 const MyReviewsCard = ({ rw, reviews, setReviews }) => {
     const { _id, name, massage, image, review, serviceName } = rw
-
+    const [upDateReview, setUpdateReview ] = useState() 
+    const [massageId, setMassageId] = useState()
 
     console.log(rw._id)
     if(rw._id){
@@ -34,7 +50,7 @@ const MyReviewsCard = ({ rw, reviews, setReviews }) => {
 
 
     const notify = () => toast("successfully deleted!");
-
+    const [modalIsOpen, setIsOpen] = React.useState(false);
     const handelDelete = (id) => {
         const confirm = window.confirm('Are you sure delete this Review')
         if (confirm) {
@@ -52,14 +68,73 @@ const MyReviewsCard = ({ rw, reviews, setReviews }) => {
                 })
         }
     }
+
+    const sendReview = (e) => {
+
+        e.preventDefault()
+        const massage = e.target.massage.value
+        
+        fetch(`http://localhost:5000/reviews/${massageId}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({massage, review : upDateReview})
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.modifiedCount > 0){
+                    closeModal()
+                }
+            })
+    }
+    
+    const ratingChanged = (newRating) => {
+        setUpdateReview(newRating)
+    }
+
+    function openModal(id) {
+        setMassageId(id)
+        setIsOpen(true);
+    }
+
+
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
         <div>
             <ToastContainer />
 
 
+            <ReactModal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+        >
+            <div className="flex flex-col max-w-xl p-8 shadow-sm rounded-xl lg:p-12 dark:bg-gray-100 dark:text-gray-800">
+                <form onSubmit={sendReview} className="flex flex-col items-center w-full">
+                    <h2 className="text-3xl font-semibold text-center">Your opinion matters!</h2>
+
+                    <StarsRating
+                        count={5}
+                        onChange={ratingChanged}
+                        size={32}
+                        color2={'#f97316'} />,
+                    
+                    <div className="flex flex-col w-full">
+                        <textarea rows="3" name='massage' placeholder="Message..." className="p-4 rounded-md resize-none dark:text-gray-800 dark:bg-gray-50"></textarea>
+                        <button type="submit" className="py-4 my-8 font-semibold rounded-md dark:text-gray-900 dark:bg-violet-400">Update</button>
+                    </div>
+                </form>
+                <button onClick={closeModal} className=" text-red-600 hover:text-white bg-red-200 text-sm btn-error p-3"> no change
+                </button>
+            </div>
+        </ReactModal>
 
 
-            
             {rw._id ?
             <div className="container flex flex-col w-full my-5  max-w-lg p-6 mx-auto divide-y rounded-md bg-slate-50 shadow-lg">
                 <div>
@@ -85,7 +160,7 @@ const MyReviewsCard = ({ rw, reviews, setReviews }) => {
                     <p>{massage}</p>
                 </div>
                 <div className='flex justify-between pt-2'>
-                    <button onClick={() => handelEdit(_id)} className='btn btn-sm'>edit</button>
+                    <button onClick={() => openModal(_id)} className='btn btn-sm'>edit</button>
                     <button onClick={() => handelDelete(_id)} className='btn btn-sm'>delete</button>
                 </div>
             </div>: <p>No Data</p>}
